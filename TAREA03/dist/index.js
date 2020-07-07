4057c2e66d75a7155612eb194fba14d74d08e6db
DaviD <david.albornoz@unmsm.edu.pe>
2020-07-07 10:52:30 -0500

    Revisión

--- TAREA03/dist/index.js
+++ TAREA03/dist/index.js
@@ -1,7 +1,11 @@
 import { Computer } from './class/Computer.js';
 import { Proforma } from './class/Proforma.js';
 import { allDB } from './ComponentesDB/AllDB.js';
-import { render } from './utils/render.js';
+import { CPU_DB } from './ComponentesDB/CPU_DB.js';
+import { InputDevicesDB } from './ComponentesDB/InputDevicesDB.js';
+import { OutputDevicesDB } from './ComponentesDB/OutputDevicesDB.js';
+import { SpecialDevicesDB } from './ComponentesDB/SpecialDevicesDB.js';
+//import { addComponent } from '../index.js'
 let computer = new Computer();
 let proforma = new Proforma();
 let IdsAddedComponents = [];
@@ -18,6 +22,144 @@ let $Modal_Image = document.querySelector('#Modal-Image');
 let $Modal_Img = document.createElement('img');
 $Modal_Img.classList.add('my-4', 'w-90px');
 let $Modal_Btn = document.querySelector('#Modal-button');
+function renderCPUs() {
+    let CPUs = [...CPU_DB];
+    let $itemsDP = document.querySelector('#itemsDP');
+    for (const info of CPUs) {
+        let card = document.createElement('div');
+        card.classList.add('card', 'col-sm-4');
+        card.style;
+        let car_body = document.createElement('div');
+        car_body.classList.add('card-body');
+        let card_title = document.createElement('h5');
+        card_title.classList.add('card-title');
+        card_title.style.fontSize = '15px';
+        card_title.textContent = `${info.name} ${info.brand} ${info.model} `;
+        let img = document.createElement('img');
+        img.classList.add('mb-2');
+        img.style.width = '90px';
+        let card_price = document.createElement('p');
+        card_price.classList.add('card-text');
+        card_price.textContent = `S/.${info.price}`;
+        let btnAdd = document.createElement('button');
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2');
+        btnAdd.textContent = 'Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id = "nbutton";
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsDP.appendChild(card);
+    }
+}
+function renderInputDevices() {
+    let InputDevices = [...InputDevicesDB];
+    let $itemsID = document.querySelector('#itemsID');
+    for (const info of InputDevices) {
+        let card = document.createElement('div');
+        card.classList.add('card', 'col-sm-4');
+        card.style;
+        let car_body = document.createElement('div');
+        car_body.classList.add('card-body');
+        let card_title = document.createElement('h5');
+        card_title.classList.add('card-title');
+        card_title.style.fontSize = '15px';
+        card_title.textContent = `${info.name} ${info.brand} ${info.model} `;
+        let img = document.createElement('img');
+        img.classList.add('mb-2');
+        img.style.width = '90px';
+        let card_price = document.createElement('p');
+        card_price.classList.add('card-text');
+        card_price.textContent = `S/.${info.price}`;
+        let btnAdd = document.createElement('button');
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2');
+        btnAdd.textContent = 'Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id = "nbutton";
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsID.appendChild(card);
+    }
+}
+function renderOutputDevices() {
+    let OutputDevices = [...OutputDevicesDB];
+    let $itemsOD = document.querySelector('#itemsOD');
+    for (const info of OutputDevices) {
+        let card = document.createElement('div');
+        card.classList.add('card', 'col-sm-4');
+        card.style;
+        let car_body = document.createElement('div');
+        car_body.classList.add('card-body');
+        let card_title = document.createElement('h5');
+        card_title.classList.add('card-title');
+        card_title.style.fontSize = '15px';
+        card_title.textContent = `${info.name} ${info.brand} ${info.model} `;
+        let img = document.createElement('img');
+        img.classList.add('mb-2');
+        img.style.width = '90px';
+        let card_price = document.createElement('p');
+        card_price.classList.add('card-text');
+        card_price.textContent = `S/.${info.price}`;
+        let btnAdd = document.createElement('button');
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2');
+        btnAdd.textContent = 'Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id = "nbutton";
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsOD.appendChild(card);
+    }
+}
+function renderSpecialDevices() {
+    let SpecialDevices = [...SpecialDevicesDB];
+    let $itemsSD = document.querySelector('#itemsSD');
+    for (const info of SpecialDevices) {
+        let card = document.createElement('div');
+        card.classList.add('card', 'col-sm-4');
+        card.style;
+        let car_body = document.createElement('div');
+        car_body.classList.add('card-body');
+        let card_title = document.createElement('h5');
+        card_title.classList.add('card-title');
+        card_title.style.fontSize = '15px';
+        card_title.textContent = `${info.name} ${info.brand} ${info.model} `;
+        let img = document.createElement('img');
+        img.classList.add('mb-2');
+        img.style.width = '90px';
+        let card_price = document.createElement('p');
+        card_price.classList.add('card-text');
+        card_price.textContent = `S/.${info.price}`;
+        let btnAdd = document.createElement('button');
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2');
+        btnAdd.textContent = 'Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id = "nbutton";
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsSD.appendChild(card);
+    }
+}
+export function render() {
+    renderCPUs();
+    renderInputDevices();
+    renderOutputDevices();
+    renderSpecialDevices();
+}
 render();
 function renderProforma() {
     // Id de los componentes renderizados en proforma:
@@ -68,23 +210,35 @@ export function addComponent() {
     computer.addComponent(allDB[IdNumber - 1]);
     renderProforma();
 }
-function renderModal() {
-    $Modal_Btn.classList.remove('btn-outline-danger');
-    $Modal_Btn.classList.remove('btn-outline-success');
+/*function renderModal() {
+    $Modal_Btn.classList.remove('btn-outline-danger')
+    $Modal_Btn.classList.remove('btn-outline-success')
     if (computer.isValidate) {
-        $Modal_Message.textContent = 'The minimum computer requirements WERE MET.';
-        $Modal_Img.src = 'https://image.flaticon.com/icons/svg/391/391175.svg';
-        $Modal_Btn.classList.add('btn-outline-success');
+        $Modal_Message.textContent='The minimum computer requirements WERE MET.';
+        $Modal_Img.src='https://image.flaticon.com/icons/svg/391/391175.svg'
+        $Modal_Btn.classList.add('btn-outline-success')
+    }else{
+        $Modal_Message.textContent='The minimum computer requirements WERE NOT MET.';
+        $Modal_Img.src='https://image.flaticon.com/icons/svg/391/391116.svg'
+        $Modal_Btn.classList.add('btn-outline-danger')
     }
-    else {
-        $Modal_Message.textContent = 'The minimum computer requirements WERE NOT MET.';
-        $Modal_Img.src = 'https://image.flaticon.com/icons/svg/391/391116.svg';
-        $Modal_Btn.classList.add('btn-outline-danger');
-    }
-    $Modal_Content.appendChild($Modal_Message);
-    $Modal_Image.appendChild($Modal_Img);
-}
+    $Modal_Content.appendChild($Modal_Message)
+    $Modal_Image.appendChild($Modal_Img)
+}*/
+let m = document.querySelector("#Message");
+let p = document.createElement("p");
+m.appendChild(p);
 function verify() {
     computer.iValidator();
-    renderModal();
+    //let v = computer.isValidate
+    p.textContent = "";
+    if (computer.isValidate) {
+        console.log(" Es correcto");
+        p.textContent = "Su compra es correcta";
+    }
+    else {
+        console.log(" Faltan componentes");
+        p.textContent = "Faltan componentes";
+    }
+    //renderModal()
 }
--- TAREA03/src/index.ts
+++ TAREA03/src/index.ts
@@ -2,7 +2,16 @@ import { Computer } from './class/Computer.js'
 import { Proforma } from './class/Proforma.js'
 import { allDB } from './ComponentesDB/AllDB.js'
 
-import { render } from './utils/render.js'
+//import { render } from './utils/render.js'
+
+import { Component } from './class/component.js'
+
+import { CPU_DB } from './ComponentesDB/CPU_DB.js'
+import { InputDevicesDB } from './ComponentesDB/InputDevicesDB.js'
+import { OutputDevicesDB } from './ComponentesDB/OutputDevicesDB.js'
+import { SpecialDevicesDB } from'./ComponentesDB/SpecialDevicesDB.js'
+
+//import { addComponent } from '../index.js'
 
 let computer = new Computer()
 let proforma = new Proforma()
@@ -23,6 +32,182 @@ let $Modal_Img = document.createElement('img') as HTMLImageElement;
 $Modal_Img.classList.add('my-4','w-90px')
 let $Modal_Btn = document.querySelector('#Modal-button') as HTMLButtonElement;
 
+function renderCPUs(){
+    let CPUs: Component[] = [...CPU_DB]
+    let $itemsDP = document.querySelector('#itemsDP') as HTMLDivElement;
+    for (const info of CPUs) {
+
+        let card = document.createElement('div') as HTMLDivElement;
+        card.classList.add('card', 'col-sm-4');
+        card.style
+
+        let car_body = document.createElement('div') as HTMLDivElement;
+        car_body.classList.add('card-body');
+
+        let card_title = document.createElement('h5') as HTMLHeadingElement;
+        card_title.classList.add('card-title');
+        card_title.style.fontSize='15px';
+        card_title.textContent=`${info.name} ${info.brand} ${info.model} `;
+        
+        let img = document.createElement('img') as HTMLImageElement;
+        img.classList.add('mb-2')
+        img.style.width='90px';
+      
+        let card_price = document.createElement('p') as HTMLParagraphElement;
+        card_price.classList.add('card-text');
+        card_price.textContent=`S/.${info.price}`;
+
+        let btnAdd = document.createElement('button') as HTMLButtonElement;
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2')
+        btnAdd.textContent='Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id="nbutton";
+
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsDP.appendChild(card);
+    }
+}
+
+function renderInputDevices() {
+    let InputDevices: Component[] = [...InputDevicesDB]
+    let $itemsID = document.querySelector('#itemsID') as HTMLDivElement;
+
+    for (const info of InputDevices) {
+        let card = document.createElement('div') as HTMLDivElement;
+        card.classList.add('card', 'col-sm-4');
+        card.style
+
+        let car_body = document.createElement('div') as HTMLDivElement;
+        car_body.classList.add('card-body');
+
+        let card_title = document.createElement('h5') as HTMLHeadingElement;
+        card_title.classList.add('card-title');
+        card_title.style.fontSize='15px';
+        card_title.textContent=`${info.name} ${info.brand} ${info.model} `;
+
+        let img = document.createElement('img') as HTMLImageElement;
+        img.classList.add('mb-2')
+        img.style.width='90px';
+
+        
+
+        let card_price = document.createElement('p') as HTMLParagraphElement;
+        card_price.classList.add('card-text');
+        card_price.textContent=`S/.${info.price}`;
+
+        let btnAdd = document.createElement('button') as HTMLButtonElement;
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2')
+        btnAdd.textContent='Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id="nbutton";
+
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsID.appendChild(card);
+    }
+}
+
+function renderOutputDevices() {
+    let OutputDevices: Component[] = [...OutputDevicesDB]
+    let $itemsOD = document.querySelector('#itemsOD') as HTMLDivElement;
+
+    for (const info of OutputDevices) {
+        let card = document.createElement('div') as HTMLDivElement;
+        card.classList.add('card', 'col-sm-4');
+        card.style
+
+        let car_body = document.createElement('div') as HTMLDivElement;
+        car_body.classList.add('card-body');
+
+        let card_title = document.createElement('h5') as HTMLHeadingElement;
+        card_title.classList.add('card-title');
+        card_title.style.fontSize='15px';
+        card_title.textContent=`${info.name} ${info.brand} ${info.model} `;
+
+        let img = document.createElement('img') as HTMLImageElement;
+        img.classList.add('mb-2')
+        img.style.width='90px';
+       
+
+        let card_price = document.createElement('p') as HTMLParagraphElement;
+        card_price.classList.add('card-text');
+        card_price.textContent=`S/.${info.price}`;
+
+        let btnAdd = document.createElement('button') as HTMLButtonElement;
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2')
+        btnAdd.textContent='Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id="nbutton";
+
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsOD.appendChild(card);
+    }
+}
+
+
+
+function renderSpecialDevices() {
+    let SpecialDevices: Component[] = [...SpecialDevicesDB]
+    let $itemsSD = document.querySelector('#itemsSD') as HTMLDivElement;
+    for (const info of SpecialDevices) {
+        let card = document.createElement('div') as HTMLDivElement;
+        card.classList.add('card', 'col-sm-4');
+        card.style
+
+        let car_body = document.createElement('div') as HTMLDivElement;
+        car_body.classList.add('card-body');
+
+        let card_title = document.createElement('h5') as HTMLHeadingElement;
+        card_title.classList.add('card-title');
+        card_title.style.fontSize='15px';
+        card_title.textContent=`${info.name} ${info.brand} ${info.model} `;
+
+        let img = document.createElement('img') as HTMLImageElement;
+        img.classList.add('mb-2')
+        img.style.width='90px';
+      
+
+        let card_price = document.createElement('p') as HTMLParagraphElement;
+        card_price.classList.add('card-text');
+        card_price.textContent=`S/.${info.price}`;
+
+        let btnAdd = document.createElement('button') as HTMLButtonElement;
+        btnAdd.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-2')
+        btnAdd.textContent='Añadir al Carrito';
+        btnAdd.setAttribute('identificador', info['id']);
+        btnAdd.addEventListener('click', addComponent);
+        btnAdd.id="nbutton";
+        
+
+        car_body.appendChild(card_title);
+        car_body.appendChild(img);
+        car_body.appendChild(card_price);
+        car_body.appendChild(btnAdd);
+        card.appendChild(car_body);
+        $itemsSD.appendChild(card);
+    }
+}
+
+export function render() {
+    renderCPUs()
+    renderInputDevices()
+    renderOutputDevices()
+    renderSpecialDevices()
+}
 render()
 
 function renderProforma() {
@@ -84,7 +269,7 @@ export function addComponent(this: HTMLButtonElement) {
     renderProforma()
 }
 
-function renderModal() {
+/*function renderModal() {
     $Modal_Btn.classList.remove('btn-outline-danger')
     $Modal_Btn.classList.remove('btn-outline-success')
     if (computer.isValidate) {
@@ -98,9 +283,24 @@ function renderModal() {
     }
     $Modal_Content.appendChild($Modal_Message)
     $Modal_Image.appendChild($Modal_Img)
-}
-
+}*/
+let m = document.querySelector("#Message") as HTMLDivElement
+let p = document.createElement("p") as HTMLParagraphElement
+m.appendChild(p)
 function verify() {
     computer.iValidator()
-    renderModal()
+    
+    //let v = computer.isValidate
+    p.textContent = "";
+    if ( computer.isValidate )   {
+        console.log(" Es correcto")
+        p.textContent = "Su compra es correcta"
+    }else{
+        console.log(" Faltan componentes")
+        p.textContent = "Faltan componentes"
+        
+
+    }
+    //renderModal()
+    
 }
